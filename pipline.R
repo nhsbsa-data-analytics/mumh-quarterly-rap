05db2cd3dad33e2f950760064cce9cf5a75e0926
GPlatten <32270079+GPlatten@users.noreply.github.com>
2022-02-24 13:28:22 +0000

    Pipeline update to write and format Excel file

--- pipline.R
+++ pipline.R
@@ -178,7 +178,190 @@ logr::put(model_data)
 
 # 8. write data to .xlsx --------------------------------------------------
 
-# TODO
+# create dataframe for full patient identification
+patient_identification_excel <- raw_data$quarterly %>%
+  tidyr::pivot_wider(
+    names_from = IDENTIFIED_FLAG,
+    values_from = c(ITEM_COUNT, ITEM_PAY_DR_NIC, PATIENT_COUNT)
+  ) %>%
+  dplyr::mutate(
+    RATE = round(ITEM_COUNT_Y/(ITEM_COUNT_Y + ITEM_COUNT_N) * 100, 10)
+  ) %>%
+  select(
+    FINANCIAL_QUARTER,
+    `BNF Section Name` = SECTION_NAME,
+    `BNF Section Code` = SECTION_CODE,
+    RATE
+  ) %>%
+  tidyr::pivot_wider(
+    names_from = FINANCIAL_QUARTER,
+    values_from = RATE
+  ) %>%
+  dplyr::arrange(`BNF Section Code`)
+
+logr::put(patient_identification_excel)
+
+# create wb object
+# create list of sheetnames needed (overview and metadata created automatically)
+sheetNames <- c("Patient_Identification",
+                "Monthly_Data",
+                "Quarterly_Data")
+
+wb <- create_wb(sheetNames)
+
+#create metadata tab (will need to open file and auto row heights once ran)
+meta_fields <- c("BNF Section Code",
+                 "BNF Section Name",
+                 "Financial Year",
+                 "Year Month",
+                 "Financial Quarter",
+                 "Identified Patient",
+                 "Total Items",
+                 "Total Net Ingredient Cost (GBP)",
+                 "Total Patients"
+)
+meta_descs <- c("The unique code used to refer to the British National Formulary (BNF) section.",
+                "The name given to a British National Formulary (BNF) section. This is the next broadest grouping of the BNF therapeutical classification system after chapter.",
+                "The financial year to which the data belongs.",
+                "The year and month to which the data belongs, denoted in YYYYMM format.",
+                "The financial quarter to which the data belongs.",
+                "This shows where an item has been attributed to an NHS number that has been verified by the Personal Demographics Service (PDS).",
+                "The number of prescription items dispensed. 'Items' is the number of times a product appears on a prescription form. Prescription forms include both paper prescriptions and electronic messages.",
+                "Total Net Ingredient Cost is the amount that would be paid using the basic price of the prescribed drug or appliance and the quantity prescribed. Sometimes called the 'Net Ingredient Cost' (NIC). The basic price is given either in the Drug Tariff or is determined from prices published by manufacturers, wholesalers or suppliers. Basic price is set out in Parts 8 and 9 of the Drug Tariff. For any drugs or appliances not in Part 8, the price is usually taken from the manufacturer, wholesaler or supplier of the product. This is given in GBP (£).",
+                "Where patients are identified via the flag, the number of patients that the data corresponds to. This will always be 0 where 'Identified Patient' = N."
+)
+create_metadata(wb,
+                meta_fields,
+                meta_descs
+)
+
+#### Patient identification
+# write data to sheet
+write_sheet(wb,
+            "Patient_Identification",
+            "Medicines Used in Mental Health - Quarterly Summary Statistics April 2015 to December 2021 - Proportion of items for which an NHS number was recorded (%)",
+            c("The below proportions reflect the percentage of prescription items where a PDS verified NHS number was recorded."),
+            patient_identification_excel,
+            42)
+
+#left align columns A and B
+format_data(wb,
+            "Patient_Identification",
+            c("A", "B"),
+            "left",
+            "")
+
+#right align columns and round to 2 DP - C to AC (!!NEED TO UPDATE AS DATA EXPANDS!!)
+format_data(wb,
+            "Patient_Identification",
+            c("C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+              "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+              "AA", "AB", "AC"),
+            "right",
+            "0.00")
+
+#### Monthly data
+# write data to sheet
+write_sheet(wb,
+            "Monthly_Data",
+            "Medicines Used in Mental Health - Quarterly Summary Statistics April 2015 to December 2021 totals by year month",
+            c("1. Field definitions can be found on the 'Metadata' tab.",
+              "2. Patient counts should not be aggregated to any other level than that which is displayed to prevent multiple counting of patients."),
+            raw_data$monthly %>%
+              rename(`Financal Year` = FINANCIAL_YEAR,
+                     `Financial Quarter` = FINANCIAL_QUARTER,
+                     `Year Month` = YEAR_MONTH,
+                     `BNF Section Name` = SECTION_NAME,
+                     `BNF Section Code` = SECTION_CODE,
+                     `Identified Patient` = IDENTIFIED_FLAG,
+                     `Total Patients` = PATIENT_COUNT,
+                     `Total Items` = ITEM_COUNT,
+                     `Total Net Ingredient Cost (GBP)` = ITEM_PAY_DR_NIC) %>%
+              select(`Financal Year`,
+                     `Financial Quarter`,
+                     `Year Month`,
+                     `BNF Section Name`,
+                     `BNF Section Code`,
+                     `Identified Patient`,
+                     `Total Patients`,
+                     `Total Items`,
+                     `Total Net Ingredient Cost (GBP)`),
+            14)
+
+#left align columns A to F
+format_data(wb,
+            "Monthly_Data",
+            c("A", "B", "C", "D", "E", "F"),
+            "left",
+            "")
+
+#right align columns G and H and round to whole numbers with thousand separator
+format_data(wb,
+            "Monthly_Data",
+            c("G", "H"),
+            "right",
+            "#,##0")
+
+#right align column I and round to 2dp with thousand separator
+format_data(wb,
+            "Monthly_Data",
+            c("I"),
+            "right",
+            "#,##0.00")
+
+#### Quarterly data
+# write data to sheet
+write_sheet(wb,
+            "Quarterly_Data",
+            "Medicines Used in Mental Health - Quarterly Summary Statistics April 2015 to December 2021 totals by quarter",
+            c("1. Field definitions can be found on the 'Metadata' tab.",
+              "2. Patient counts should not be aggregated to any other level than that which is displayed to prevent multiple counting of patients."),
+            raw_data$quarterly %>%
+              rename(`Financal Year` = FINANCIAL_YEAR,
+                     `Financial Quarter` = FINANCIAL_QUARTER,
+                     `BNF Section Name` = SECTION_NAME,
+                     `BNF Section Code` = SECTION_CODE,
+                     `Identified Patient` = IDENTIFIED_FLAG,
+                     `Total Patients` = PATIENT_COUNT,
+                     `Total Items` = ITEM_COUNT,
+                     `Total Net Ingredient Cost (GBP)` = ITEM_PAY_DR_NIC) %>%
+              select(`Financal Year`,
+                     `Financial Quarter`,
+                     `BNF Section Name`,
+                     `BNF Section Code`,
+                     `Identified Patient`,
+                     `Total Patients`,
+                     `Total Items`,
+                     `Total Net Ingredient Cost (GBP)`),
+            14)
+
+#left align columns A to F
+format_data(wb,
+            "Quarterly_Data",
+            c("A", "B", "C", "D", "E"),
+            "left",
+            "")
+
+#right align columns G and H and round to whole numbers with thousand separator
+format_data(wb,
+            "Quarterly_Data",
+            c("F", "G"),
+            "right",
+            "#,##0")
+
+#right align column I and round to 2dp with thousand separator
+format_data(wb,
+            "Quarterly_Data",
+            c("H"),
+            "right",
+            "#,##0.00")
+
+#save file into outputs folder
+openxlsx::saveWorkbook(wb,
+                       "outputs/mumh_quarterly_dec21_v001.xlsx",
+                       overwrite = TRUE)
+
+
 
 
 # 9. render markdown ------------------------------------------------------
